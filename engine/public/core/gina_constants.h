7c10c868663a3c5f1be026edad5f908ce629e47d
Maryan Yaroma <yaroma.maryan@gmail.com>
2025-04-03 22:14:47 +0200

    Added constants stub.

---
+++ engine/public/core/gina_constants.h
@@ -0,0 +1,13 @@
+#ifndef _GINA_CONSTANTS_H_
+#define _GINA_CONSTANTS_H_
+
+#include "core/gina_types.h"
+
+namespace gina
+{
+    constexpr bool ENABLE_DEBUG_LAYER = true;
+    constexpr bool USE_WARP_DEVICE = false;
+    constexpr uint32 BUFFER_COUNT = 2;
+}
+
+#endif // !_GINA_CONSTANTS_H_
